9c1db412a58417c54a1647dac2b7c312c2b1a05f
Sean Capelle <seancapelle@gmail.com>
2016-07-18 21:54:35 -0400

    Updated objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,19 @@
 var timer = 10;
+
+var question = [{
+	question: "Between Greedo and Han, who shot first?",
+	answers: ["Han", "Greedo", "They shot at the same time", "Not sure"],
+	correctAnswer: "Han"
+}, {
+	question: "How old is Yoda when he dies?",
+	answers: ["300 years", "500 years", "900 years", "A millennium"],
+	correctAnswer: "900 years"
+}, {
+	question: "Which Rebel spaceship is considered a mid-range ship?",
+	answers: ["A-Wing", "B-Wing", "X-Wing", "Y-Wing"],
+	correctAnswer: "X-Wing"
+}];
+
 document.querySelector('#timer').innerHTML = timer;
 
 var countDown = function(){
@@ -15,12 +30,27 @@ var countDown = function(){
 
 var nextQuestion = function(){
 
+
+for (i = 0; i < question.length; i++){
+	
+	console.log(i);
+
+	var qDisplay = question[i].question;
+	document.querySelector('#qDisplay').innerHTML = qDisplay;
+
+	var aDisplay = question[i].answers;
+	document.querySelector('#aDisplay').innerHTML = aDisplay;
+
+	var correct = question[i].correctAnswer;
+	
+	};
 }
 
-
+//Start of game
 $(document).ready(function(){
 
 setInterval(countDown, 1000);
+nextQuestion();
 
 
 })
