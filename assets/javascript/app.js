52c1383752ff292d2f206285c97c54584eb6b45d
Sean Capelle <seancapelle@gmail.com>
2016-07-20 14:54:54 -0400

    Added function to game object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-var timer = 10;
-var answer = $('#aDisplay');
-
+//Questions object
 var question = [{
 	question: "Between Greedo and Han, who shot first?",
 	answers: ["Han", "Greedo", "They shot at the same time", "Not sure"],
@@ -13,93 +11,175 @@ var question = [{
 	question: "Which Rebel spaceship is considered a mid-range ship?",
 	answers: ["A-Wing", "B-Wing", "X-Wing", "Y-Wing"],
 	correctAnswer: "X-Wing"
+},{
+	question: "What is the name of Boba Fett's ship?",
+	answers: ["Fett-1", "Firestorm", "Slave-1", "Avenging Angel"],
+	correctAnswer: "Slave-1"
+},{
+	question: "What is the name of the planet that houses the second Death Star's shield generator?",
+	answers: ["Tatooine", "Dantooine", "Mustafar", "Endor"],
+	correctAnswer: "Endor"
+},{
+	question: "Which stoutly X-Wing pilot died during the Battle of Yavin?",
+	answers: ["Porkins", "Beefkins", "Chubbs", "Gordo"],
+	correctAnswer: "Porkins"
+},{
+	question: "What game did Han lose the Millennium Falcon over?",
+	answers: ["Chess", "Blackjack", "Sabaac", "Bocci"],
+	correctAnswer: "Sabaac"
+},{
+	question: "How did Vader plan to deliver Luke to the Emperor?",
+	answers: ["Freeze him in carbonite", "Cut him in pieces", "Shackle him", "Hypnotize him"],
+	correctAnswer: "Freeze him in carbonite"
+},{
+	question: "Who killed Jabba the Hutt?",
+	answers: ["Princess Leia", "Luke", "Chewbacca", "Darth Vader"],
+	correctAnswer: "Princess Leia"
+},{
+	question: "Who does Anakin beat in pod racing?",
+	answers: ["Jar-Jar", "Watto", "Ben Quadinaros", "Sebulba"],
+	correctAnswer: "Sebulba"
 }];
 
-document.querySelector('#timer').innerHTML = timer;
+//Game object
+var game = {
 
-var countDown = function(){
+	//Variables
+	timer: 30,
+	correctScore: 0,
+	incorrectScore: 0,
+	unanswered: 0,
 
-	if(timer > 0){
-		timer--;
-		document.querySelector('#timer').innerHTML = timer;
-	}
-	else {
-		clearInterval(countDown);
-		// location.reload();
-	}
-}
+	//Functions
+	countDown: function() {
 
-var nextQuestion = function(){
+		console.log("In countDown");
 
+		if(game.timer > 0){
+			game.timer--;
+			$('#timer').html(game.timer);
+		}
+		else {
+			clearInterval(game.countDown);
+			
+		}
+	},
 
-for (i = 0; i < question.length; i++){
+	askQuestion: function() {
+
+		console.log("In askQuestion");
+
+		//Timer counts down
+		setInterval(game.countDown, 1000);
 	
-	console.log(i);
+		//Display question
+		$('#qDisplay').html(question[0].question);
 
-	var qDisplay = question[i].question;
-	document.querySelector('#qDisplay').innerHTML = qDisplay;
+		//Set each answer to a variable
+		var answerOne = question[0].answers[0];
+		$('#answerOne').html(answerOne);
 
-	var answerOne = question[i].answers[0];
-	document.querySelector('#answerOne').innerHTML = answerOne;
+		var answerTwo = question[0].answers[1];
+		$('#answerTwo').html(answerTwo);
 
-	var answerTwo = question[i].answers[1];
-	document.querySelector('#answerTwo').innerHTML = answerTwo;
+		var answerThree = question[0].answers[2];
+		$('#answerThree').html(answerThree);
 
-	var answerThree = question[i].answers[2];
-	document.querySelector('#answerThree').innerHTML = answerThree;
+		var answerFour = question[0].answers[3];
+		$('#answerFour').html(answerFour);
 
-	var answerFour = question[i].answers[3];
-	document.querySelector('#answerFour').innerHTML = answerFour;
+		//Set correct answer
+		var correct = question[0].correctAnswer;
+	
 
-	var correct = question[i].correctAnswer;
-	};
+		//Take user guess
+		$('#answerOne').unbind().click(function() {
+			
+			userGuess = answerOne;
+			game.checkGuess(userGuess, correct);
+		})
 
-	$('#answerOne').on('click', function(){
-		
-		if (answerOne === correct){
+		$('#answerTwo').unbind().click(function() {
+			
+			userGuess = answerTwo;
+			game.checkGuess(userGuess, correct);
+		})
+
+		$('#answerThree').unbind().click(function() {
+			
+			userGuess = answerThree;
+			game.checkGuess(userGuess, correct);
+		})
+
+		$('#answerFour').unbind().click(function() {
+			
+			userGuess = answerFour;
+			game.checkGuess(userGuess, correct);
+		})
+
+	},
+
+	checkGuess: function(userGuess,correct) {
+
+		console.log("In checkGuess");
+
+		if (userGuess === correct){
 			alert("YES!");
+			game.correctScore++;
+			game.nextQuestion();
+			
 		}
 		else {
 			alert("NO!");
+			game.incorrectScore++;
+			game.nextQuestion();
 		}
-	})
+	},
 
-	$('#answerTwo').on('click', function(){
-		
-		if (answerTwo === correct){
-			alert("YES!");
-		}
-		else {
-			alert("NO!");
-		}
-	})
+	nextQuestion: function() {
 
-	$('#answerThree').on('click', function(){
-		
-		if (answerThree === correct){
-			alert("YES!");
-		}
-		else {
-			alert("NO!");
-		}
-	})
+		console.log("In nextQuestion");
 
-	$('#answerFour').on('click', function(){
-		
-		if (answerFour === correct){
-			alert("YES!");
-		}
-		else {
-			alert("NO!");
-		}
-	})
-}
+		//Set next question to index 0
+		question.shift();
+
+		game.timer = 30;
+		// window.refreshIntervalId;
+		clearInterval(game.countDown);
+
+		game.askQuestion();
+	},
+
+	finished: function() {
+		console.log(correctScore);
+		console.log(incorrectScore);
+		console.log(unanswered);
+	},
+
+	reset: function() {
+
+		location.reload();
+	},
+
+};
 
 //Start of game
-$(document).ready(function(){
+// $(document).ready(function(){
 
-setInterval(countDown, 1000);
-nextQuestion();
+game.askQuestion();
 
 
-})
+// })
+
+
+//NOTES
+//question++
+
+// if (question === question.length){
+
+// }
+
+
+
+
+// game.checkGuess();
